2c2ee3e382ccc816d8ac7c6e54e765a765bd0c92
Zac Wen <zeoh@vip.qq.com>
2016-03-24 10:29:48 +0800

    [Accepted]committed by LeetCode Extension

---
+++ 215-Kth-Largest-Element-in-an-Array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        make_heap(nums.begin(), nums.end());
+        for (int i = 0; i < k-1; i++) {
+            pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        }
+        return nums.front();
+    }
+};
