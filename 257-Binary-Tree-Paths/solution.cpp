81d32d3b3da8b97dee7e00a548f260a04c6f37d9
Zac Wen <zeoh@vip.qq.com>
2016-03-27 21:28:02 +0800

    [Accepted]committed by LeetCode Extension

---
+++ 257-Binary-Tree-Paths/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> retPaths;
+        string path = "";
+        helper(retPaths, root, path);
+        return retPaths;
+    }
+    
+private:
+    void helper(vector<string> &retPaths, TreeNode* node, string path) {
+        if(!node) return;
+        path += to_string(node->val);
+        if(node->left==NULL && node->right==NULL) {
+            retPaths.push_back(path);
+            return;
+        } else {
+            path += "->";
+            if(node->left) helper(retPaths, node->left, path);
+            if(node->right) helper(retPaths, node->right, path);
+        }
+    }
+};
