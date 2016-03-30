fb5438d8e0b420dc32d5309bc705632392d08976
Zac Wen <zeoh@vip.qq.com>
2016-03-30 18:35:19 +0800

    [171. Excel Sheet Column Number][Accepted]committed by LeetCode Extension

--- 171-Excel-Sheet-Column-Number/solution.cpp
+++ 171-Excel-Sheet-Column-Number/solution.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int titleToNumber(string s) {
-        int length = s.size();
-        if(length==0) return 0;
         int colNum = 0;
+        int length = s.size();
         for(int i = 0, exp = length-1; i < length; i++, exp--) {
             int shift = (int)(s.at(i)-'A'+1);
             colNum += shift*pow(26, exp);
