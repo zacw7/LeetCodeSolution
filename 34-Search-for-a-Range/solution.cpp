581dec52600826921dba0cbf107bb96a3642b901
Zac Wen <zac.wen7@gmail.com>
2016-05-14 09:58:35 +0800

    [34. Search for a Range][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 34-Search-for-a-Range/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> results = {-1, -1};
+        int size = nums.size();
+        int left = 0;
+        int right = size-1;
+        if(left==right && results[0]==target) {
+            results[0]=results[1]=0;
+            return results;
+        }
+        int mid=right;
+        while(left < right) {
+            if(nums[mid]==target){
+                left = right = mid;
+                break;
+            } else if(nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+            mid=left+(right-left)/2;
+        }
+        if(nums[left]==target) {
+            while(left>0 && nums[left-1]==target) left--;
+            while(right<size-1 && nums[right+1]==target) right++;
+            results[0] = left;
+            results[1] = right;
+        }
+        return results;
+    }
+};
