278dc4bae79595c5481a4e26da9e093cf9aa0634
Zac Wen <zac.wen7@gmail.com>
2016-05-20 11:45:18 +0800

    [97. Interleaving String][Time Limit Exceeded]committed by LeetCode Extension

--- 97-Interleaving-String/solution.cpp
+++ 97-Interleaving-String/solution.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         bool result = false;
-        if(s1.size()+s2.size() == s3.size()) result = helper(0, 0, 0, s1, s2, s3);
+        int size1 = s1.size();
+        int size2 = s2.size();
+        int size3 = s3.size();
+        if(size1+size2 == size3) result = helper(0, 0, 0, s1, s2, s3, size1, size2, size3);
         return result;
     }
     
-    bool helper(int p1, int p2, int p3, string &s1, string &s2, string &s3) {
-        if(p3==s3.size()) return true;
+    bool helper(int p1, int p2, int p3, string &s1, string &s2, string &s3, int size1, int size2, int size3) {
+        if(p3==size3) return true;
         bool r1, r2; 
         r1 = r2 = false;
         
-        if(p1<s1.size() && s3[p3]==s1[p1]) {
-            r1 = helper(p1+1, p2, p3+1, s1, s2, s3);
+        if(p1<size1 && s3[p3]==s1[p1]) {
+            r1 = helper(p1+1, p2, p3+1, s1, s2, s3, size1, size2, size3);
         }
-        if(p2<s2.size() && s3[p3]==s2[p2]) {
-            r2 = helper(p1, p2+1, p3+1, s1, s2, s3);
+        if(p2<size2 && s3[p3]==s2[p2]) {
+            r2 = helper(p1, p2+1, p3+1, s1, s2, s3, size1, size2, size3);
         }
         return r1||r2;
     }
