69e3110f61bc7d401583a8b6dde4a73de7e27e1e
Zac Wen <zac.wen7@gmail.com>
2016-05-20 11:47:27 +0800

    [97. Interleaving String][Time Limit Exceeded]committed by LeetCode Extension

--- 97-Interleaving-String/solution.cpp
+++ 97-Interleaving-String/solution.cpp
@@ -11,15 +11,9 @@ public:
     
     bool helper(int p1, int p2, int p3, string &s1, string &s2, string &s3, int size1, int size2, int size3) {
         if(p3==size3) return true;
-        bool r1, r2; 
-        r1 = r2 = false;
+        if(p1<size1 && s3[p3]==s1[p1] && helper(p1+1, p2, p3+1, s1, s2, s3, size1, size2, size3)) return true;
+        if(p2<size2 && s3[p3]==s2[p2] && helper(p1, p2+1, p3+1, s1, s2, s3, size1, size2, size3)) return true;
         
-        if(p1<size1 && s3[p3]==s1[p1]) {
-            r1 = helper(p1+1, p2, p3+1, s1, s2, s3, size1, size2, size3);
-        }
-        if(p2<size2 && s3[p3]==s2[p2]) {
-            r2 = helper(p1, p2+1, p3+1, s1, s2, s3, size1, size2, size3);
-        }
-        return r1||r2;
+        return false;
     }
 };
