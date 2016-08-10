43fdc1874e8a13f79b2cf2848904ab0396bb6d71
Zac Wen <zac.wen7@gmail.com>
2016-08-10 17:10:54 +0800

    [176. Second Highest Salary][Runtime Error]committed by LeetCode Extension

--- 176-Second-Highest-Salary/solution.sql
+++ 176-Second-Highest-Salary/solution.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select *
-from
-    (select *
-     from Employee
-     order by salary) A
-where rownum = 2
+SELECT distinct(Salary) AS SecondHighestSalary
+FROM Employee
+ORDER BY SecondHighestSalary
+LIMIT 1,1;
+
+SELECT distinct(Salary) as SecondHighestSalary FROM Employee UNION SELECT NULL ORDER BY SecondHighestSalary DESC LIMIT 1,1;
