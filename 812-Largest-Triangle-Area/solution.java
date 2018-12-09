894c57e26d5e87d59bcd266b0ee02ea7806fde24
Zac <zac.wen7@gmail.com>
2018-12-09 17:09:33 +0800

    812. Largest Triangle Area

---
+++ 812-Largest-Triangle-Area/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double maxArea = 0;
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    int x1 = points[i][0], y1 = points[i][1];
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int x3 = points[k][0], y3 = points[k][1];
+                    double area
+                            = Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2) / 2.0d;
+                    maxArea = (area > maxArea) ? area : maxArea;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
