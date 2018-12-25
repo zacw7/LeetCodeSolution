14277b4ed44426f8123956b95996668fd47a2bca
Zeoh <zac.wen7@gmail.com>
2018-12-25 18:29:22 +0800

    557. Reverse Words in a String III

---
+++ 917-Reverse-Only-Letters/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseOnlyLetters(String S) {
+        char[] chars = S.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            if (!((chars[left] >= 'a' && chars[left] <= 'z')
+                    || (chars[left] >= 'A' && chars[left] <= 'Z'))) {
+                left++;
+            } else if (!((chars[right] >= 'a' && chars[right] <= 'z')
+                    || (chars[right] >= 'A' && chars[right] <= 'Z'))) {
+                right--;
+            } else {
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+}
