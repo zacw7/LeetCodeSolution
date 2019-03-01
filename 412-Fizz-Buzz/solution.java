308c660bf47ea79e0a603d78621ca51edebfac44
Zeoh <zac.wen7@gmail.com>
2019-03-01 17:57:29 +0800

    412. Fizz Buzz

---
+++ 412-Fizz-Buzz/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) ans.add("FizzBuzz");
+            else if (i % 5 == 0) ans.add("Buzz");
+            else if (i % 3 == 0) ans.add("Fizz");
+            else ans.add(String.valueOf(i));
+        }
+        return ans;
+    }
+}
