388ced221047a07b57474b87fbadd07e69a1363a
Zeoh <zac.wen7@gmail.com>
2019-03-15 17:33:13 +0800

    492. Construct the Rectangle

---
+++ 492-Construct-the-Rectangle/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        for (int w = (int) Math.sqrt(area); w >= 1; w--) {
+            if (area % w == 0) return new int[]{area / w, w};
+        }
+        return new int[]{area, 1};
+    }
+}
