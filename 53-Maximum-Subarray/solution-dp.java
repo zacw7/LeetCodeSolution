0cb06a29b288a0ea2d56a7f4dd226dcb907ccc02
Zac <zac.wen7@gmail.com>
2019-04-02 22:30:38 +0800

    53. Maximum Subarray

---
+++ 53-Maximum-Subarray/solution-dp.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+}
