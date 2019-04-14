26fee4e1745cb1707c3f5be8289511fa73e61dec
Zac <zac.wen7@gmail.com>
2019-04-14 12:16:54 +0800

    33. Search in Rotated Sorted Array

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,31 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int shift = 0;
-        int low = 0, high = nums.length - 1;
-        int mid = (low + high) / 2;
-        if (nums[low] > nums[high]) {
-            // find how many numbers rotated
-            while (mid != low && mid != high && mid > 0 && nums[mid] > nums[mid - 1]) {
-                if (nums[mid] > nums[0]) low = mid;
-                if (nums[mid] < nums[0]) high = mid;
-                mid = (low + high) / 2;
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int lo = 0, hi = nums.length - 1;
+        int mid;
+
+        while (lo + 1 < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                if (nums[mid] < nums[lo] && target > nums[hi]) hi = mid;
+                else lo = mid;
+            } else {
+                if (nums[mid] > nums[hi] && target < nums[lo]) lo = mid;
+                else hi = mid;
             }
-            shift = mid;
         }
-        // check nums[high], nums[low]
-        low = shift;
-        high = shift + nums.length - 1;
-        if (target == nums[low % nums.length]) return low % nums.length;
-        if (target == nums[high % nums.length]) return high % nums.length;
-        mid = (low + high) / 2;
-        while (mid != low && mid != high) {
-            int cur = mid % nums.length;
-            if (nums[cur] == target) break;
-            if (nums[cur] < target) low = mid;
-            if (nums[cur] > target) high = mid;
-            mid = (low + high) / 2;
-        }
-        return (mid == low || mid == high) ? -1 : mid % nums.length;
+
+        if (nums[lo] == target) return lo;
+        if (nums[hi] == target) return hi;
+        return -1;
     }
 }
