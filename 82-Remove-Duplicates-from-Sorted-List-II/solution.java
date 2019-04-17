08a46d28f073bba791b30f7a91e707445a1bef65
Zeoh <zac.wen7@gmail.com>
2019-04-17 10:25:51 +0800

    82. Remove Duplicates from Sorted List II

--- 82-Remove-Duplicates-from-Sorted-List-II/solution.java
+++ 82-Remove-Duplicates-from-Sorted-List-II/solution.java
@@ -1,42 +1,29 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node prev;
-    public Node next;
-    public Node child;
-
-    public Node() {}
-
-    public Node(int _val,Node _prev,Node _next,Node _child) {
-        val = _val;
-        prev = _prev;
-        next = _next;
-        child = _child;
-    }
-};
-*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public Node flatten(Node head) {
-        Stack<Node> parents = new Stack();
-
-        Node node = head;
-        while (node != null) {
-            if (node.next == null && !parents.isEmpty()) {
-                Node parent = parents.pop();
-                node.next = parent.next;
-                if (node.next != null) node.next.prev = node;
-                parent.child.prev = parent;
-                parent.next = parent.child;
-                parent.child = null;
-            }
-            if (node.child != null) {
-                parents.push(node);
-                node = node.child;
+    // T: O(n)
+    // S: O(1)
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (prev.next != null && prev.next.next != null) {
+            if (prev.next.val == prev.next.next.val) {
+                ListNode curr = prev.next.next.next;
+                while (curr != null && curr.val == prev.next.val) {
+                    curr = curr.next;
+                }
+                prev.next = curr;
             } else {
-                node = node.next;
+                prev = prev.next;
             }
         }
-        return head;
+        return dummy.next;
     }
 }
