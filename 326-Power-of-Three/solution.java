2f0bd72d9dd624bec8d4bef6f68fc395f1906c9e
Zeoh <zac.wen7@gmail.com>
2019-04-19 15:16:29 +0800

    326. Power of Three

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) return false;
-        while (n > 1) {
-            if (n % 3 > 0) return false;
-            else n /= 3;
+        if (n <= 0) {
+            return false;
         }
-        return true;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
 }
