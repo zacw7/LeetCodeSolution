0275de5b54bad9cf1c5d0913d293b44ad1cd59f4
Zeoh <zac.wen7@gmail.com>
2019-04-22 15:04:20 +0800

    586. Customer Placing the Largest Number of Orders

--- 586-Customer-Placing-the-Largest-Number-of-Orders/solution.sql
+++ 586-Customer-Placing-the-Largest-Number-of-Orders/solution.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT t.class
-FROM (
-    SELECT class, count(distinct student) as students
-    FROM courses
-    GROUP BY class
-) t
-WHERE t.students>=5
+
+select customer_number
+from orders
+group by customer_number
+order by count(1) desc
+limit 1
