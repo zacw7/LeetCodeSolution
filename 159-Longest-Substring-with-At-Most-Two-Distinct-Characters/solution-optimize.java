dbd08d59571166342e7ec414cfbe483b0c1e10f3
Zeoh <zac.wen7@gmail.com>
2019-04-25 13:33:14 +0800

    159. Longest Substring with At Most Two Distinct Characters

---
+++ 159-Longest-Substring-with-At-Most-Two-Distinct-Characters/solution-optimize.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int count = 0;
+        int[] charCount = new int[256];
+        int maxLen = 0;
+        int left = 0, right = 0;
+        while (right <= s.length()) {
+            if (count <= 2) {
+                maxLen = Math.max(maxLen, right - left);
+                if (right < s.length()) {
+                    if (charCount[s.charAt(right)] == 0) {
+                        count++;
+                    }
+                    charCount[s.charAt(right)]++;
+                }
+                right++;
+            } else {
+                charCount[s.charAt(left)]--;
+                if (charCount[s.charAt(left)] == 0) {
+                    count--;
+                }
+                left++;
+            }
+        }
+        return maxLen;
+    }
+}
