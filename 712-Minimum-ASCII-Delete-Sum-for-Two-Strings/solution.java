a3befb1403f7391a766e8ee520823092463558e5
Zeoh <zac.wen7@gmail.com>
2019-04-25 18:05:27 +0800

    712. Minimum ASCII Delete Sum for Two Strings

---
+++ 712-Minimum-ASCII-Delete-Sum-for-Two-Strings/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    // dp
+    // T: O(mn)
+    // S: O(mn)
+    public int minimumDeleteSum(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return 0;
+        }
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
+        }
+        for (int j = 1; j <= s2.length(); j++) {
+            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                            Math.min(dp[i - 1][j] + s1.charAt(i - 1), dp[i][j - 1] + s2.charAt(j - 1)));
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + s1.charAt(i - 1) + s2.charAt(j - 1),
+                            Math.min(dp[i - 1][j] + s1.charAt(i - 1), dp[i][j - 1] + s2.charAt(j - 1)));
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
