87f78fd56e3199ee4f407ca05420c90e6f32a98c
Zac <zac.wen7@gmail.com>
2019-05-01 09:48:14 +0800

    735. Asteroid Collision

--- 735-Asteroid-Collision/solution.java
+++ 735-Asteroid-Collision/solution.java
@@ -3,24 +3,13 @@ class Solution {
         Stack<Integer> stack = new Stack();
 
         for (int cur : asteroids) {
-            if (!stack.isEmpty() && stack.peek() > 0 && cur < 0) {
-                int prev = stack.pop();
-                if (prev == -cur) {
-                    continue;
-                } else if (prev > -cur){
-                    stack.push(prev);
-                } else {
-                    while (!stack.isEmpty() && stack.peek() > 0 && -cur > stack.peek()) {
-                        stack.pop();
-                    }
-                    if (stack.isEmpty() || stack.peek() < 0) {
-                        stack.push(cur);
-                    } else if (stack.peek() == -cur) {
-                        stack.pop();
-                    }
-                }
-            } else {
+            while (!stack.isEmpty() && stack.peek() > 0 && cur < 0 && -cur > stack.peek()) {
+                stack.pop();
+            }
+            if (cur > 0 || stack.isEmpty() || stack.peek() < 0) {
                 stack.push(cur);
+            } else if (stack.peek() == -cur) {
+                stack.pop();
             }
         }
 
