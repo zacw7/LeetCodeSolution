f4d42813821134a1df23af476773fa52f29bab3d
Zeoh <zac.wen7@gmail.com>
2019-05-10 16:55:05 +0800

    317. Shortest Distance from All Buildings

--- 317-Shortest-Distance-from-All-Buildings/solution.java
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -3,38 +3,41 @@ class Solution {
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
             return -1;
         }
-        int BLDG_ID = 0;
         int R = grid.length, C = grid[0].length;
-        Queue<int[]> queue = new LinkedList();
+        List<int[]> buildings = new LinkedList();
         for (int r = 0; r < R; r++) {
             for (int c = 0; c < C; c++) {
                 if (grid[r][c] == 1) {
-                    queue.add(new int[]{r, c, BLDG_ID++});
+                    buildings.add(new int[]{r, c});
                 }
             }
         }
-        boolean[][][] visited = new boolean[R][C][BLDG_ID];
         int[][] distSums = new int[R][C];
         int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int dist = 0, minDist = Integer.MAX_VALUE;
-        while (!queue.isEmpty()) {
-            dist++;
-            int s = queue.size();
-            while (s-- > 0) {
-                int[] cur = queue.poll();
-                int id = cur[2];
-                int r = cur[0], c = cur[1];
-                visited[r][c][id] = true;
-                for (int[] d : directions) {
-                    r = cur[0] + d[0];
-                    c = cur[1] + d[1];
-                    if (r >= 0 && r < R && c >= 0 && c < C && !visited[r][c][id] && grid[r][c] <= 0) {
-                        visited[r][c][id] = true;
-                        grid[r][c]--;
-                        distSums[r][c] += dist;
-                        queue.offer(new int[]{r, c, id});
-                        if (grid[r][c] == -BLDG_ID) {
-                            minDist = Math.min(minDist, distSums[r][c]);
+        int minDist = Integer.MAX_VALUE;
+        for (int[] b : buildings) {
+            boolean[][] visited = new boolean[R][C];
+            int dist = 0;
+            Queue<int[]> queue = new LinkedList();
+            queue.offer(b);
+            while (!queue.isEmpty()) {
+                dist++;
+                int s = queue.size();
+                while (s-- > 0) {
+                    int[] cur = queue.poll();
+                    int r = cur[0], c = cur[1];
+                    visited[r][c] = true;
+                    for (int[] d : directions) {
+                        r = cur[0] + d[0];
+                        c = cur[1] + d[1];
+                        if (r >= 0 && r < R && c >= 0 && c < C && !visited[r][c] && grid[r][c] <= 0) {
+                            visited[r][c] = true;
+                            grid[r][c]--;
+                            distSums[r][c] += dist;
+                            queue.offer(new int[]{r, c});
+                            if (grid[r][c] == -buildings.size()) {
+                                minDist = Math.min(minDist, distSums[r][c]);
+                            }
                         }
                     }
                 }
