17f7be2c29c139dea8f792643c3dacbad3ac8e44
Zeoh <zac.wen7@gmail.com>
2019-05-14 18:12:04 +0800

    652. Find Duplicate Subtrees

--- 652-Find-Duplicate-Subtrees/solution.java
+++ 652-Find-Duplicate-Subtrees/solution.java
@@ -9,27 +9,23 @@
  */
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        Map<String, List<TreeNode>> nodeMap = new HashMap();
-        serialize(root, nodeMap);
-        List<TreeNode> duplicates = new ArrayList();
-        for (List<TreeNode> nodeList : nodeMap.values()) {
-            if (nodeList.size() > 1) {
-                duplicates.add(nodeList.get(0));
-            }
-        }
-        return duplicates;
+        Map<String, Integer> nodeMap = new HashMap();
+        List<TreeNode> ans = new ArrayList();
+        serialize(root, nodeMap, ans);
+        return ans;
     }
 
-    private String serialize(TreeNode node, Map<String, List<TreeNode>> nodeMap) {
+    private String serialize(TreeNode node, Map<String, Integer> nodeMap, List<TreeNode> ans) {
         if (node == null) {
             return "";
         }
         String serializeNode = String.valueOf(node.val);
-        serializeNode = serializeNode + "L" + serialize(node.left, nodeMap);
-        serializeNode = serializeNode + "R" + serialize(node.right, nodeMap);
-        List<TreeNode> nodeList = nodeMap.getOrDefault(serializeNode, new ArrayList());
-        nodeList.add(node);
-        nodeMap.put(serializeNode, nodeList);
+        serializeNode = serializeNode + "L" + serialize(node.left, nodeMap, ans);
+        serializeNode = serializeNode + "R" + serialize(node.right, nodeMap, ans);
+        nodeMap.put(serializeNode, nodeMap.getOrDefault(serializeNode, 0) + 1);
+        if (nodeMap.get(serializeNode) == 2) {
+            ans.add(node);
+        }
         return serializeNode;
     }
 }
