577208f3f2995e465749cfd41836ea45d570f0bd
Zeoh <zac.wen7@gmail.com>
2019-06-03 16:20:21 +0800

    78. Subsets

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
-        int subsetCount = 1;
-        for (int i = 0; i < nums.length; i++) subsetCount *= 2;
-        subsetCount -= 1;
-        while (subsetCount >= 0) {
-            List<Integer> subset = new ArrayList();
-            for (int comb = subsetCount, i = 0; comb > 0 && i < nums.length; i++, comb >>= 1) {
-                if ((comb & 1) > 0) subset.add(nums[i]);
-            }
-            res.add(subset);
-            subsetCount--;
+        List<List<Integer>> subsets = new ArrayList();
+        subsetHelper(nums, new ArrayList(), 0, subsets);
+        return subsets;
+    }
+
+    private void subsetHelper(int[] nums, List<Integer> curr, int index, List<List<Integer>> subsets) {
+        if (index == nums.length) {
+            subsets.add(curr);
+            return;
         }
-        return res;
+        subsetHelper(nums, new ArrayList(curr), index + 1, subsets);
+        curr.add(nums[index]);
+        subsetHelper(nums, curr, index + 1, subsets);
     }
 }
