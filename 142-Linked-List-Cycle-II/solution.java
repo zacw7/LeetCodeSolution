bcd176fcf8f8572ed68f640e139b6f5e4715c76a
Zac <zac.wen7@gmail.com>
2019-06-05 15:01:31 +0800

    142. Linked List Cycle II

--- 142-Linked-List-Cycle-II/solution.java
+++ 142-Linked-List-Cycle-II/solution.java
@@ -11,26 +11,21 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-
-        ListNode slow = head.next, fast = head.next.next;
-        while (fast != null && fast.next != null && fast != slow) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
         }
-
         if (fast == null || fast.next == null) {
             return null;
         }
-
-        ListNode meetNode = slow, node = head;
-        while (meetNode != node) {
-            meetNode = meetNode.next;
-            node = node.next;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
         }
-
-        return node;
+        return head;
     }
 }
