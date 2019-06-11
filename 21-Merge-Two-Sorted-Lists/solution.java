ab334e39d11acc00725023c0a675ff6770a8a199
Zeoh <zac.wen7@gmail.com>
2019-06-11 17:04:00 +0800

    update solutions

--- 21-Merge-Two-Sorted-Lists/solution.java
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -7,34 +7,27 @@
  * }
  */
 class Solution {
-    // T: O(m + n)
+    // T: O(min(m, n))
     // S: O(1)
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-
-        ListNode dummy = new ListNode(0), curr = dummy;
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
         while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                curr.next = l1;
+            if (l1.val <= l2.val) {
+                node.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;
+                node.next = l2;
                 l2 = l2.next;
             }
-            curr = curr.next;
+            node = node.next;
         }
-
         if (l1 != null) {
-            curr.next = l1;
-        } else if (l2 != null) {
-            curr.next = l2;
+            node.next = l1;
         }
-
+        if (l2 != null) {
+            node.next = l2;
+        }
         return dummy.next;
     }
 }
--- 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
+++ 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        Map<Integer, Integer> sumMap = new HashMap();
-        sumMap.put(0, 0);
-        int maxLen = 0;
-        for (int i = 0, sum = 0; i < nums.length; i++) {
+        Map<Integer, Integer> prefixSumMap = new HashMap();
+        prefixSumMap.put(0, -1);
+        int sum = 0, max = 0;
+        for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (sumMap.containsKey(sum - k)) {
-                maxLen = Math.max(maxLen, i + 1 - sumMap.get(sum - k));
+            if (prefixSumMap.containsKey(sum - k)) {
+                max = Math.max(max, i - prefixSumMap.get(sum - k));
             }
-            if (!sumMap.containsKey(sum)) {
-                sumMap.put(sum, i + 1);
+            if (!prefixSumMap.containsKey(sum)) {
+                prefixSumMap.put(sum, i);
             }
         }
-        return maxLen;
+        return max;
     }
 }
