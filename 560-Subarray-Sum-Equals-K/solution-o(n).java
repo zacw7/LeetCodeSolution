e98e524b55473047c4a1b93b07781a33897c0180
Zeoh <zac.wen7@gmail.com>
2019-06-11 17:56:02 +0800

    update solutions

--- 560-Subarray-Sum-Equals-K/solution-o(n).java
+++ 560-Subarray-Sum-Equals-K/solution-o(n).java
@@ -1,13 +1,12 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap();
-        map.put(0, 1);
-        int count = 0;
-        for (int i = 1, prev = 0; i <= nums.length; i++) {
-            int sum = prev + nums[i - 1];
-            prev = sum;
-            if (map.containsKey(sum - k)) count += map.get(sum - k);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        Map<Integer, Integer> prefixSumCount = new HashMap();
+        prefixSumCount.put(0, 1);
+        int sum = 0, count = 0;
+        for (int n : nums) {
+            sum += n;
+            count += prefixSumCount.getOrDefault(sum - k, 0);
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
