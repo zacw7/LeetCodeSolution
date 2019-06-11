92bf0cba01ca1112511fc9128dde508e15df77b1
Zeoh <zac.wen7@gmail.com>
2019-06-11 16:34:15 +0800

    update solution

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,33 +1,32 @@
 class MedianFinder {
 
-    private Queue<Integer> minHeap, maxHeap;
+    PriorityQueue<Integer> minHeap, maxHeap;
 
     /** initialize your data structure here. */
     public MedianFinder() {
         this.minHeap = new PriorityQueue();
-        this.maxHeap = new PriorityQueue<Integer>((o1, o2) -> o2 - o1);
+        this.maxHeap = new PriorityQueue(Collections.reverseOrder());
     }
 
     public void addNum(int num) {
-        if (minHeap.size() < maxHeap.size()) minHeap.add(num);
-        else maxHeap.add(num);
-        if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-            while (maxHeap.peek() > minHeap.peek()) {
-                minHeap.add(maxHeap.poll());
-                maxHeap.add(minHeap.poll());
-            }
+        if (minHeap.isEmpty() || num >= minHeap.peek()) {
+            minHeap.offer(num);
+        } else {
+            maxHeap.offer(num);
         }
     }
 
     public double findMedian() {
-        if ((maxHeap.size() + minHeap.size()) % 2 == 1) return maxHeap.peek();
-        else return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        while (!maxHeap.isEmpty() && minHeap.size() < maxHeap.size()) {
+            minHeap.offer(maxHeap.poll());
+        }
+        while (!minHeap.isEmpty() && maxHeap.size() + 1 < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+        if (minHeap.size() > maxHeap.size()) {
+            return minHeap.peek() * 1.0;
+        } else {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
