76e79a56622a810356ee5c2310e06f33b5d6ff4d
Zeoh <zac.wen7@gmail.com>
2019-07-02 12:50:17 +0800

    573. Squirrel Simulation

--- 573-Squirrel-Simulation/solution.java
+++ 573-Squirrel-Simulation/solution.java
@@ -1,42 +1,12 @@
 class Solution {
     public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
-        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Map<Integer, Integer> nutsIndex = new HashMap();
-        for (int i = 0; i < nuts.length; i++) {
-            nutsIndex.put(nuts[i][0] * width + nuts[i][1], i);
+        int dist = 0, delta = Integer.MAX_VALUE;
+        for (int[] n : nuts) {
+            int treeDist = Math.abs(tree[0] - n[0]) + Math.abs(tree[1] - n[1]);
+            int sqrlDist = Math.abs(squirrel[0] - n[0]) + Math.abs(squirrel[1] - n[1]);
+            dist += 2 * treeDist;
+            delta = Math.min(delta, sqrlDist - treeDist);
         }
-        int[] treeDist = new int[nuts.length];
-        boolean[] visited = new boolean[height * width];
-        Queue<Integer> queue = new LinkedList();
-        queue.offer(tree[0] * width + tree[1]);
-        visited[tree[0] * width + tree[1]] = true;
-        int dist = 0, diff = Integer.MAX_VALUE;
-        while (!queue.isEmpty() && !nutsIndex.isEmpty()) {
-            dist++;
-            int s = queue.size();
-            while (s-- > 0 && !nutsIndex.isEmpty()) {
-                int pos = queue.poll();
-                for (int[] d : dir) {
-                    int x = pos / width + d[0], y = pos % width + d[1];
-                    int cur = x * width + y;
-                    if (x >= 0 && x < height && y >= 0 && y < width && !visited[cur]) {
-                        visited[cur] = true;
-                        queue.offer(cur);
-                        if (nutsIndex.containsKey(cur)) {
-                            treeDist[nutsIndex.get(cur)] = dist;
-                            nutsIndex.remove(cur);
-                            int sqrlDist = Math.abs(x - squirrel[0]) + Math.abs(y - squirrel[1]);
-                            diff = Math.min(diff, sqrlDist - dist);
-                        }
-                    }
-                }
-            }
-        }
-        int sum = 0;
-        for (int d : treeDist) {
-            sum += d * 2;
-        }
-        sum = sum + diff;
-        return sum;
+        return dist + delta;
     }
 }
