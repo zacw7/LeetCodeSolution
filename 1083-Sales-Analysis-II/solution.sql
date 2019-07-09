795109ff92b495c20f8d252ab1fd4e240c1cbddb
Zeoh <zac.wen7@gmail.com>
2019-07-09 18:07:26 +0800

    1083. Sales Analysis II

--- 1083-Sales-Analysis-II/solution.sql
+++ 1083-Sales-Analysis-II/solution.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-SELECT buyer_id
-FROM Sales s JOIN Product p ON s.product_id=p.product_id
-GROUP BY buyer_id
-HAVING SUM(CASE WHEN product_name='S8' THEN 1 ELSE 0 END) > 0
-AND SUM(CASE WHEN product_name='iPhone' THEN 1 ELSE 0 END) = 0
+SELECT DISTINCT buyer_id
+FROM Sales
+WHERE buyer_id IN (
+    SELECT buyer_id
+    FROM Sales s JOIN Product p ON s.product_id=p.product_id
+    WHERE p.product_name='S8'
+)
+AND buyer_id NOT IN (
+    SELECT buyer_id
+    FROM Sales s JOIN Product p ON s.product_id=p.product_id
+    WHERE p.product_name='iPhone'
+)
