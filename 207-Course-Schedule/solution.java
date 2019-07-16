98aee399260b88a0e4fb9ad971fc03ab4a3cccd2
Zeoh <zac.wen7@gmail.com>
2019-07-16 18:24:11 +0800

    update solutions

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,37 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
+        int[] indegrees = new int[numCourses];
         List<Integer>[] adjList = new List[numCourses];
-
-        for (int[] pre : prerequisites) {
-            inDegree[pre[0]]++;
-            if (adjList[pre[1]] == null) {
-                adjList[pre[1]] = new ArrayList();
-            }
-            adjList[pre[1]].add(pre[0]);
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList();
         }
-
-        while (numCourses > 0) {
-            int pick = -1;
-            for (int i = 0; i < inDegree.length; i++) {
-                if (inDegree[i] == 0) {
-                    pick = i;
-                    inDegree[i] = -1;
-                    if (adjList[i] != null) {
-                        for (int n : adjList[i]) {
-                            inDegree[n]--;
-                        }
-                    }
-                    break;
+        for (int[] p : prerequisites) {
+            indegrees[p[0]]++;
+            adjList[p[1]].add(p[0]);
+        }
+        Queue<Integer> queue = new LinkedList();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int neighbor : adjList[cur]) {
+                if (indegrees[neighbor] == 1) {
+                    queue.offer(neighbor);
                 }
-            }
-            if (pick == -1) {
-                break;
-            } else {
-                numCourses--;
+                indegrees[neighbor]--;
             }
         }
-
-        return numCourses == 0;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
