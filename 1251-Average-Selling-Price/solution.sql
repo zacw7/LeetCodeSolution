ab42eb7c1767c064db458605de6c7c34c2c8aa74
Zac <zac.wen7@gmail.com>
2019-11-11 12:02:02 -0800

    1251. Average Selling Price

---
+++ 1251-Average-Selling-Price/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT u.product_id, ROUND(SUM(u.units * p.price) / SUM(u.units), 2) average_price
+FROM UnitsSold u
+JOIN Prices p
+ON u.product_id=p.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY u.product_id
