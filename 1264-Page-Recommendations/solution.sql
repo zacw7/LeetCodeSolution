7d71a352b30f106123110879eebddf1f27408d9e
Zac <zac.wen7@gmail.com>
2019-11-24 15:45:15 -0800

    1264. Page Recommendations

---
+++ 1264-Page-Recommendations/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT DISTINCT(page_id) recommended_page
+FROM Likes
+WHERE page_id NOT IN (
+    SELECT page_id FROM Likes WHERE user_id=1
+  )
+  AND user_id IN (
+      SELECT user1_id FROM Friendship WHERE user2_id=1
+      UNION
+      SELECT user2_id FROM Friendship WHERE user1_id=1
+  )
