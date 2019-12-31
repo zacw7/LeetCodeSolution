0f7f983bbbe9ce7dbd360f6667d9a659e363fa01
Zac <zac.wen7@gmail.com>
2019-12-31 13:25:26 -0800

    1302. Deepest Leaves Sum

---
+++ 1302-Deepest-Leaves-Sum/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        if (root != null) {
+            queue.offer(root);
+        }
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            sum = 0;
+            while (sz-- > 0) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return sum;
+    }
+}
